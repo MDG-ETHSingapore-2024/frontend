88b8d449a24fc72a4d2e7a579c96c603e99b76f6
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-09-21 00:27:53 +0800

    chore: shadcn, magicui and aceternity setup
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { toast } from "sonner";
 import "./global.css";
+import { NeonGradientCard } from "./components/magicui/neon-gradient-card";
+import { CardContainer } from "./components/ui/3d-card";
 function App() {
   return (
-    <>
-      <h1 className="text-red-700 bg-black">something</h1>
-    </>
+    <div className="flex flex-col justify-center items-center h-screen w-screen">
+      <CardContainer>
+        <NeonGradientCard className="w-96 h-40 ">
+          <h1 className="text-2xl">Let's get toasty 💋💋💋</h1>
+          <button
+            onClick={() => {
+              toast.error("We fucked up !!!");
+            }}
+          >
+            Toast me
+          </button>
+        </NeonGradientCard>
+      </CardContainer>
+    </div>
   );
 }
 
