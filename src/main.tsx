7da447df3aabe9ace4c6087490147a89592b4701
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-09-21 06:15:29 +0800

    chore: redux toolkit setup
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Toaster } from "./components/ui/sonner.tsx";
+import { ReduxProvider } from "./redux/provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-    <Toaster />
+    <ReduxProvider>
+      <App />
+      <Toaster />
+    </ReduxProvider>
   </StrictMode>
 );
