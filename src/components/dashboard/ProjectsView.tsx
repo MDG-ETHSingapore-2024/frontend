5f6c12095184ed014ea25826b8a43ee988de25b5
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-09-21 19:27:34 +0800

    feat: create project modal
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

--- src/components/dashboard/ProjectsView.tsx
+++ src/components/dashboard/ProjectsView.tsx
@@ -1,17 +1,9 @@
 import { Project } from "@/utils/types";
 import ProjectCard from "./ProjectCard";
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "../ui/pagination";
-import { Link } from "react-router-dom";
-import { useEffect } from "react";
 
+import { PaginationView } from "./PagtinationView";
+import { useState } from "react";
+import CreateProjectModal from "./ProjectModal";
 const ProjectView = ({
   projects,
   index,
@@ -19,11 +11,17 @@ const ProjectView = ({
   projects: Project[];
   index: number;
 }) => {
+  const [isNewProject, setIsNewProject] = useState<boolean>(false);
   return (
     <div className="flex px-40 py-10 h-full flex-col gap-10">
       <div className="flex justify-between">
         <h1 className="text-2xl">Projects</h1>
-        <button className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+        <button
+          onClick={() => {
+            setIsNewProject(true);
+          }}
+          className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+        >
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#1ADBCC_0%,#FFD700_50%,#1ADBCC_100%)]" />
           <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-[#DB1A5A] px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
             Create Project
@@ -35,52 +33,16 @@ const ProjectView = ({
           <ProjectCard key={project.id} project={project} />
         ))}
       </div>
-      <PaginationView pages={projects.length / 6} current={index ? index : 1} />
+      <PaginationView
+        pages={Math.ceil(projects.length / 6)}
+        current={index ? index : 1}
+      />
+      <CreateProjectModal
+        isOpen={isNewProject}
+        onClose={() => setIsNewProject(false)}
+      />
     </div>
   );
 };
-
-export function PaginationView({
-  pages,
-  current,
-}: {
-  pages: number;
-  current: number;
-}) {
-  return (
-    <Pagination>
-      <PaginationContent>
-        {current - 1 >= 1 && (
-          <Link to={"/dashboard"} state={{ page: current - 1 }}>
-            <PaginationItem>
-              <PaginationPrevious />
-            </PaginationItem>
-          </Link>
-        )}
-        {Array(pages)
-          .fill(null)
-          .map((_, index) => (
-            <PaginationItem key={index}>
-              <Link to={"/dashboard"} state={{ page: index + 1 }}>
-                <PaginationLink
-                  className={`${index + 1 == current ? "bg-[#251926]" : ""}`}
-                  isActive={index + 1 == current}
-                >
-                  {index + 1}
-                </PaginationLink>
-              </Link>
-            </PaginationItem>
-          ))}
-        {current + 1 <= pages && (
-          <Link to={"/dashboard"} state={{ page: current + 1 }}>
-            <PaginationItem>
-              <PaginationNext />
-            </PaginationItem>
-          </Link>
-        )}
-      </PaginationContent>
-    </Pagination>
-  );
-}
 
 export default ProjectView;
