ac3af1f031ca6d657c08f0b475c921cb830f0adf
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-09-22 01:28:19 +0800

    feat: auth checks and styling
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,10 +1,16 @@
 import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
-import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
-import { Navigate, redirect } from "react-router-dom";
+import {
+  DynamicEmbeddedAuthFlow,
+  DynamicWidget,
+} from "@dynamic-labs/sdk-react-core";
+import { Navigate } from "react-router-dom";
 import { useAccount } from "wagmi";
+import confetti from "canvas-confetti";
+import { useState } from "react";
 
 const Main = () => {
   const { address } = useAccount();
+
   if (address) return <Navigate to={"/dashboard"} />;
   return (
     <BackgroundBeamsWithCollision className="min-h-screen w-full flex flex-col">
@@ -23,10 +29,86 @@ const Main = () => {
           </div>
         </div>
       </h2>
-      {/* </ShineBorder> */}
-      <DynamicWidget />
+      <ConfettiEmoji />
     </BackgroundBeamsWithCollision>
   );
 };
 
+export function ConfettiEmoji() {
+  const [isConnect, setIsConnect] = useState<boolean>(false);
+  const handleClick = (e: any) => {
+    e.stopPropagation();
+    const scalar = 2;
+    const noogles = ["⌐🎇-🎇", "⌐◨-◨", "⌐💀-💀", "⌐❍-❍", "⌐⍥-⍥"];
+    const colors = ["red", "yellow", "red", "white", "blue"];
+    const shapes = noogles.map((noggle, index) =>
+      confetti.shapeFromText({
+        text: noggle as string,
+        scalar,
+        color: colors[index],
+      })
+    );
+
+    const defaults = {
+      spread: 120,
+      ticks: 200,
+      gravity: 1,
+      decay: 0.96,
+      startVelocity: 30,
+      shapes: shapes,
+      scalar,
+    };
+
+    const shoot = () => {
+      confetti({
+        ...defaults,
+        particleCount: 30,
+      });
+
+      confetti({
+        ...defaults,
+        particleCount: 5,
+      });
+
+      confetti({
+        ...defaults,
+        particleCount: 15,
+        scalar: scalar / 2,
+        shapes: ["circle"],
+      });
+    };
+
+    setTimeout(shoot, 0);
+    setTimeout(shoot, 100);
+    setTimeout(shoot, 200);
+    setTimeout(() => {
+      setIsConnect(true);
+    }, 2000);
+  };
+
+  return (
+    <div className="relative justify-center">
+      <button
+        onClick={handleClick}
+        className="bg-white text-black py-2 px-3 rounded-xl"
+      >
+        Connect
+      </button>
+      {isConnect && (
+        <div className="fixed inset-0 z-20 bg-[#232323] bg-opacity-50 flex items-center justify-center backdrop-blur-sm">
+          <div className="bg-white rounded-lg  max-w-md w-full flex flex-col">
+            <button
+              onClick={() => setIsConnect(false)}
+              className="border-[1px] border-slate-400 text-stone-800 mb-10 right-0 w-fit self-end p-2 mt-2 mr-2 rounded-lg"
+            >
+              Cancel
+            </button>
+            <DynamicEmbeddedAuthFlow />
+            <DynamicWidget />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
 export default Main;
