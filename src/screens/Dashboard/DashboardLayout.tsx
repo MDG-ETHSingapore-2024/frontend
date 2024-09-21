3f75c216f5291f5360f18532cf3b80758a0157ee
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-09-21 20:58:33 +0800

    feat: noun UI changes
    Signed-off-by: Karthik Ayangar <karthik.ayangar7118@gmail.com>

--- src/screens/Dashboard/DashboardLayout.tsx
+++ src/screens/Dashboard/DashboardLayout.tsx
@@ -1,9 +1,14 @@
 import Navbar from "@/components/global/Navbar";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
-export const DashboardLayout = () => (
-  <div className="h-screen">
-    <Navbar />
-    <Outlet />
-  </div>
-);
+export const DashboardLayout = () => {
+  const connectedWallet = localStorage.getItem("dynamic_connected_wallet_ns");
+  if (!connectedWallet) return <Navigate to={"/"} />;
+
+  return (
+    <div className="h-screen">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
